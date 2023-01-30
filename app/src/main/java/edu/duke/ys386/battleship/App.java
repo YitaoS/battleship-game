36050161b51c19a7437b15ca6d52f8d38701ad3b
ys386 <ys386@duke.edu>
2023-01-30 01:55:59 -0500

    task 16 done

--- app/src/main/java/edu/duke/ys386/battleship/App.java
+++ app/src/main/java/edu/duke/ys386/battleship/App.java
@@ -6,43 +6,25 @@ package edu.duke.ys386.battleship;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.io.Reader;
 
 public class App {
-  final Board<Character> theBoard;
-  final BoardTextView view;
-  final BufferedReader inputReader;
-  final PrintStream out;
-  final AbstractShipFactory<Character> shipFactory;
+  TextPlayer player1;
+  TextPlayer player2;
 
-  public App(Board<Character> theBoard, Reader inputSource, PrintStream out) {
-    this.theBoard = theBoard;
-    this.view = new BoardTextView(theBoard);
-    this.inputReader = new BufferedReader(inputSource);
-    this.out = out;
-    this.shipFactory = new V1ShipFactory();
-  }
-
-  public Placement readPlacement(String prompt) throws IOException {
-    out.println(prompt);
-    String s = inputReader.readLine();
-    return new Placement(s);
-  }
-
-  public void doOnePlacement() throws IOException {
-    String prompt = "Where would you like to put your ship?";
-    Placement p = readPlacement(prompt);
-    Ship<Character> s = shipFactory.makeDestroyer(p);
-    theBoard.tryAddShip(s);
-    prompt = view.displayMyOwnBoard();
-    out.println(prompt);
+  public App(TextPlayer player1, TextPlayer player2) {
+    this.player1 = player1;
+    this.player2 = player2;
   }
 
   public static void main(String[] args) throws IOException {
-    Board<Character> theBoard = new BattleShipBoard<>(10, 20);
-    InputStreamReader isr = new InputStreamReader(System.in);
-    App app = new App(theBoard, isr, System.out);
-    app.doOnePlacement();
+    Board<Character> b1 = new BattleShipBoard<Character>(10, 20);
+    Board<Character> b2 = new BattleShipBoard<Character>(10, 20);
+    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    V1ShipFactory factory = new V1ShipFactory();
+    TextPlayer p1 = new TextPlayer("A", b1, input, System.out, factory);
+    TextPlayer p2 = new TextPlayer("B", b2, input, System.out, factory);
+    App app = new App(p1, p2);
+    p1.doPlacementPhase();
+    p2.doPlacementPhase();
   }
 }
---
+++ app/src/test/java/edu/duke/ys386/battleship/TextPlayerTest.java
@@ -0,0 +1,81 @@
+package edu.duke.ys386.battleship;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.StringReader;
+
+import org.junit.jupiter.api.Test;
+
+public class TextPlayerTest {
+  @Test
+  void test_read_placement() throws IOException {
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    TextPlayer player = createTextPlayer(10, 20, "B2V\nC8H\na4v\n", bytes);
+    String prompt = "Please enter a location for a ship:";
+    Placement[] expected = new Placement[3];
+    expected[0] = new Placement(new Coordinate(1, 2), 'V');
+    expected[1] = new Placement(new Coordinate(2, 8), 'H');
+    expected[2] = new Placement(new Coordinate(0, 4), 'V');
+    for (int i = 0; i < expected.length; i++) {
+      Placement p = player.readPlacement(prompt);
+      assertEquals(p, expected[i]); // did we get the right Placement back
+      assertEquals(prompt + "\n", bytes.toString()); // should have printed prompt and newline
+      bytes.reset(); // clear out bytes for next time around
+    }
+
+  }
+
+  @Test
+  void test_do_one_placement() throws IOException {
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    TextPlayer player = createTextPlayer(10, 20, "B2V\nC8H\na4v\n", bytes);
+    player.doOnePlacement();
+    AbstractShipFactory<Character> asf = new V1ShipFactory();
+    Board<Character> tb = new BattleShipBoard<>(10, 20);
+    Ship<Character> rts = asf.makeDestroyer(new Placement("B2V"));
+    tb.tryAddShip(rts);
+    BoardTextView btv = new BoardTextView(tb);
+    String s = "Player A where would you like to put your Destroyer?\n" + btv.displayMyOwnBoard() + "\n";
+    assertEquals(bytes.toString(), s);
+  }
+
+  private TextPlayer createTextPlayer(int w, int h, String inputData, OutputStream bytes) {
+    BufferedReader input = new BufferedReader(new StringReader(inputData));
+    PrintStream output = new PrintStream(bytes, true);
+    Board<Character> board = new BattleShipBoard<Character>(w, h);
+    V1ShipFactory shipFactory = new V1ShipFactory();
+    return new TextPlayer("A", board, input, output, shipFactory);
+  }
+
+  @Test
+  void test_do_placement_phase() throws IOException {
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    TextPlayer player = createTextPlayer(10, 20, "B2V\nC8H\na4v\n", bytes);
+    player.doPlacementPhase();
+    AbstractShipFactory<Character> asf = new V1ShipFactory();
+    Board<Character> tb = new BattleShipBoard<>(10, 20);
+    Ship<Character> rts = asf.makeDestroyer(new Placement("B2V"));
+    tb.tryAddShip(rts);
+    BoardTextView btv = new BoardTextView(tb);
+    String prompt = "--------------------------------------------------------------------------------\n" +
+        "Player A: you are going to place the following ships (which are all\n" +
+        "rectangular). For each ship, type the coordinate of the upper left\n" +
+        "side of the ship, followed by either H (for horizontal) or V (for\n" +
+        "vertical).  For example M4H would place a ship horizontally starting\n" +
+        "at M4 and going to the right.  You have\n\n " +
+        "2 \"Submarines\" ships that are 1x2\n" +
+        "3 \"Destroyers\" that are 1x3\n" +
+        "3 \"Battleships\" that are 1x4\n" +
+        "2 \"Carriers\" that are 1x6\n" +
+        "--------------------------------------------------------------------------------\n\n";
+
+    String s = prompt + "Player A where would you like to put your Destroyer?\n" + btv.displayMyOwnBoard() + "\n";
+    assertEquals(bytes.toString(), s);
+
+  }
+}
